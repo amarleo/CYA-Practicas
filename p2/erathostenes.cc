32d5ebbe40dae083a00d9ab777fab9a52f5373f3
usuario <alu0101015941@ull.edu.es>
2020-10-12 17:42:05 +0100

    almost done

--- p2/erathostenes.cc
+++ p2/erathostenes.cc
@@ -17,9 +17,48 @@
 void write (std::vector<std::pair<int, bool>> lista) {
 
    for (unsigned i = 0; i < lista.size(); i++) {
-      std::cout << lista[i].first << " " << lista[i].second << " |" << std::endl;
+      if (lista[i].second == true)
+      std::cout << lista[i].first << " ";
     }
+    std::cout << std::endl;
 } 
+
+std::vector<std::pair<int, bool>> remove (std::vector<std::pair<int, bool>> lista, int i) {
+
+    if (lista[i-1].second == true) {
+      for (unsigned j = i*i; j <= lista.size(); j += i) {
+        
+        lista[j-1].second = false;
+        
+      }
+    }
+  return lista;
+}
+
+std::vector<std::pair<int, bool>> sieve (std::vector<std::pair<int, bool>> lista) {
+
+  for (unsigned i = 2; i*i <= lista.size(); i++) {
+    lista = remove(lista, i);
+    
+  }
+  return lista;
+}
+
+std::vector<std::pair<int, bool>> delete_no_primes (std::vector<std::pair<int, bool>> lista) {
+
+   
+   for (unsigned i = 0; i < lista.size(); i++) {
+     
+      if (lista[i].second == false)
+        
+        lista.erase(lista.begin() + i);
+    }
+    
+ 
+  
+  return lista;
+} 
+
 
 int main (int argc, char *argv[]) {
 
@@ -28,6 +67,8 @@ int main (int argc, char *argv[]) {
   std::vector< std::pair<int, bool> > lista;
   lista = rellenar(numeroMax);
  
- 
+  lista = sieve(lista);
+  lista = delete_no_primes(lista);
+  write(lista);
     
 }
