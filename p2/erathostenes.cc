ec274e77737966923dab375bd8237f952341c9a9
usuario <alu0101015941@ull.edu.es>
2020-10-12 18:06:59 +0100

    almost done

--- p2/erathostenes.cc
+++ p2/erathostenes.cc
@@ -3,72 +3,58 @@
 #include <stdlib.h>
 #include <bits/stdc++.h> 
 
- std::vector<std::pair<int, bool>> rellenar(int numero) {
+std::vector<std::pair<int, bool>> CreateVector(int list_size) {    
     
-    std::vector< std::pair <int, bool>> lista;
-
-    for (int i = 0; i < numero; i++) {
-      lista.push_back({i+1, true});
-    }
-   
-    return lista;
-} 
-
-void write (std::vector<std::pair<int, bool>> lista) {
-
-   for (unsigned i = 0; i < lista.size(); i++) {
-      if (lista[i].second == true)
-      std::cout << lista[i].first << " ";
-    }
-    std::cout << std::endl;
-} 
-
-std::vector<std::pair<int, bool>> remove (std::vector<std::pair<int, bool>> lista, int i) {
-
-    if (lista[i-1].second == true) {
-      for (unsigned j = i*i; j <= lista.size(); j += i) {
-        
-        lista[j-1].second = false;
-        
-      }
-    }
-  return lista;
-}
-
-std::vector<std::pair<int, bool>> sieve (std::vector<std::pair<int, bool>> lista) {
-
-  for (unsigned i = 2; i*i <= lista.size(); i++) {
-    lista = remove(lista, i);
-    
+  std::vector< std::pair <int, bool>> vector_list;
+  for (int i = 0; i < list_size; i++) {
+    vector_list.push_back({i+1, true});
   }
-  return lista;
-}
-
-std::vector<std::pair<int, bool>> delete_no_primes (std::vector<std::pair<int, bool>> lista) {
-
-   
-   for (unsigned i = 0; i < lista.size(); i++) {
-     
-      if (lista[i].second == false)
-        
-        lista.erase(lista.begin() + i);
-    }
-    
- 
-  
-  return lista;
+  return vector_list;
 } 
 
+void Write (std::vector<std::pair<int, bool>> vector_list) {
+
+  for (unsigned i = 0; i < vector_list.size(); i++) {
+    if (vector_list[i].second == true)
+    std::cout << vector_list[i].first << " ";
+  }
+  std::cout << std::endl;
+} 
+
+std::vector<std::pair<int, bool>> Remove(std::vector<std::pair<int, bool>> vector_list, int i) {
+
+  if (vector_list[i-1].second == true) {
+    for (unsigned j = i*i; j <= vector_list.size(); j += i) {
+      vector_list[j-1].second = false;  
+    } 
+  }
+  return vector_list;
+}
+
+std::vector<std::pair<int, bool>> Sieve(std::vector<std::pair<int, bool>> vector_list) {
+
+  for (unsigned i = 2; i*i <= vector_list.size(); i++) {
+    vector_list = Remove(vector_list, i);    
+  }
+  return vector_list;
+}
+
+std::vector<std::pair<int, bool>> DeleteNoPrimes(std::vector<std::pair<int, bool>> vector_list) {
+   
+  for (unsigned i = 0; i < vector_list.size(); i++) {   
+    if (vector_list[i].second == false)      
+      vector_list.erase(vector_list.begin() + i);
+    }
+  return vector_list;
+} 
 
 int main (int argc, char *argv[]) {
 
-  int numeroMax = atoi(argv[1]);
-
-  std::vector< std::pair<int, bool> > lista;
-  lista = rellenar(numeroMax);
- 
-  lista = sieve(lista);
-  lista = delete_no_primes(lista);
-  write(lista);
+  int max_number = atoi(argv[1]);
+  std::vector< std::pair<int, bool> > vector_list;
+  vector_list = CreateVector(max_number); 
+  vector_list = Sieve(vector_list);
+  vector_list = DeleteNoPrimes(vector_list);
+  write(vector_list);
     
 }
