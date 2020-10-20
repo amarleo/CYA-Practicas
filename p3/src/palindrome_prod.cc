65ee7a2ebcccc9614d6f90152dc8f7e444a3c303
usuario <alu0101015941@ull.edu.es>
2020-10-20 20:13:31 +0100

    Manejo de errores actualizado

--- p3/src/palindrome_prod.cc
+++ p3/src/palindrome_prod.cc
@@ -1,4 +1,6 @@
 /*
+Copyright 2020 Alejandro Martín de León
+
 - Práctica 3: Productos capicúa
 
 Centro: Escuela Superior de Ingeniería y Tecnología
@@ -8,15 +10,12 @@ Autor: Alejandro Martín de León
 Contacto: alu0101015941@ull.edu.es
 
 Fichero: palindrome_prod.cc
-
-
-Copyright 2020 Alejandro Martín de León
 */
 #include <iostream>
 #include <cctype>
 #include <fstream>
 
-bool isPalindrome(int number) {
+bool IsPalindrome(int number) {
   int digit, reverse = 0, number_initial = number;
   do {
       digit = number % 10;
@@ -31,31 +30,60 @@ bool isPalindrome(int number) {
   }
 }
 
-void outputFile(char *outfile, int i, int j, int resultado) {
+void OutputFile(char *outfile, int i, int j, int resultado) {
     std::ofstream output;
     output.open(outfile, std::fstream::out | std::fstream::app);
     output << i << " * " << j << " = " << resultado << "\n";
+}
+
+void help() {
+  std::cout << "--- AYUDA ---\nPara ejecutar el programa es necesario pasar los siguientes argumentos" << std::endl;
+  std::cout << "./palindrome [NUMERO CIFRAS] [FICHERO TXT]" << std::endl;
+  std::cout << "- [NUMERO CIFRAS]: Número > 0 a elegir el número de cifras" << std::endl;
+  std::cout << "- [FICHERO TXT]: Fichero con la extención .txt, por ejemplo: fichero.txt" << std::endl;
+}
+
+bool ProgramErrors(int number_elements, char *outfile) {
+
+  if (number_elements < 3) {
+    std::cout << "ERROR: Argumentos insuficientes" << std::endl;
+    help();
+    return true;
+  }
+  else if (number_elements > 3) {
+    std::cout << "ERROR: Se han pasado demasiados argumentos." << std::endl;
+    help();
+    return true;
+  }
+  else {
+     if (number_elements <= 0 || isdigit(number_elements) == 1) {
+      std::cout << "ERROR: el número introducido es erróneo. Por favor, introduzca un número entero en el rango N > 0" << std::endl;
+      return true;
+    }
+    return false;
+  }
 }
 
 int main(int argc, char *argv[]) {
   int number = std::atoi(argv[1]);
   char *outfile = argv[2];
   std::remove(outfile);
-  if (number <= 0 || isdigit(number) == 1) {
-      std::cout << "ERROR: el número introducido es erróneo. Por favor, introduzca un número entero en el rango N > 0" << std::endl;
-      return 0;
+  if (ProgramErrors(argc, outfile) == true) {
+    return 1;
   }
+ 
   int numero_cifras = 1;
-  for (int i = 1; i <= number ; i++) {
+  for (int i = 1; i <= number ; i++) { // 10 ^ n_cifras
     numero_cifras *= 10;
   }
   int resultado = 0;
   for (int i = 1; i < numero_cifras; i++) {
     for (int j = 1; j < numero_cifras; j++) {
       resultado = i * j;
-      if ((isPalindrome(resultado) == true) && (resultado > 9)) {
-        outputFile(outfile, i, j, resultado);
+      if ((IsPalindrome(resultado) == true) && (resultado > 9)) {
+        OutputFile(outfile, i, j, resultado);
       }
     }
   }
+  return 0;
 }
